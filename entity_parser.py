358c32707e6dcae332a282c18aedfab4766e72e8
deminovamv <deminovamv@gmail.com>
2021-06-13 21:32:18 +0300

    new file

---
+++ entity_parser.py
@@ -0,0 +1,44 @@
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsNERTagger,
+    AddrExtractor,
+    Doc
+)
+
+
+class EntityParser:
+
+    def __init__(self, replica: str):
+        self.replica = replica.title()
+        self.doc = Doc(self.replica)
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        self.addr_extractor = AddrExtractor(self.morph_vocab)
+        emb = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(emb)
+        self.ner_tagger = NewsNERTagger(emb)
+
+
+    def parser(self):
+        self.doc.segment(self.segmenter)
+        self.doc.tag_morph(self.morph_tagger)
+        self.doc.tag_ner(self.ner_tagger)
+        for span in self.doc.spans: 
+            span.normalize(self.morph_vocab)
+        data = {}
+        data['LOC'] = []
+        for span in self.doc.spans:
+            if span.type == 'LOC':
+                data['LOC'].append(span.normal)
+        if not data['LOC']:
+            match = self.addr_extractor.find(self.replica)
+            if match:
+                city = self.replica[match.start:match.stop]
+                for token in self.doc.tokens:
+                    token.lemmatize(self.morph_vocab)
+                    if token.text == city:
+                        data['LOC'].append(token.lemma.title())
+        return(data)
---
+++ get_weather.py
@@ -0,0 +1,50 @@
+import requests
+import entity_parser
+import dictionary
+import settings
+
+
+class WeatherIntent():
+    
+
+    def translit(self, city_name: str):
+        for key in city_name:
+             city_name = city_name.replace(key, dictionary.letters_dict[key])
+        return city_name
+
+    
+    def det_weather(self, replica: str):
+        
+        replica = entity_parser.EntityParser(text)
+        location = replica.parser()
+        city = location['LOC']
+        if not city:
+            return'В вашей фразе я не смог распознать город'   
+        else:
+            city_name = city[0]
+            letter_city_name = city_name[0]
+            if  dictionary.city_dict[letter_city_name].get(city_name):
+                city_name = dictionary.city_dict[letter_city_name][city_name]
+                city = city_name
+                city_name = self.translit(city_name)
+                try:
+                    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                    params={'q': city_name, 'units': 'metric', 'lang': 'ru', 'APPID': settings.appid})
+                    data = res.json()
+                    answer = "Город: " + str(city)+\
+                        "; На улице " + str(data['weather'][0]['description'])+\
+                        "; Температура "+ str(data['main']['temp']) +\
+                        "; Ощущается как "+ str(data['main']['feels_like'])
+                    return answer
+                except Exception as e:
+                    if data['cod'] == '404':
+                        return(f'Город {city} не найден.')
+                    else:
+                        return'Технические неполадки, пожалуйста, попробуйте позднее.', 
+            else:
+                return'В вашей фразе я не смог распознать город'   
+
+if __name__ == "__main__":
+    test = WeatherIntent()
+    text = 'сколько градусов в мск'
+    print(test.det_weather(text))
