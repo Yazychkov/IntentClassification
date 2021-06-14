17bd8d9cfba9b9390f73bfc861cd9a0a3b92407f
deminovamv <deminovamv@gmail.com>
2021-06-14 14:05:23 +0300

    formatted files, no proxy

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import telegram_connector
-import settings
 import logging
 
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
 
-PROXY = {
-    'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': 
-        {
-            'username': settings.PROXY_USERNAME, 
-            'password': settings.PROXY_PASSWORD
-        }
-}
+# PROXY = {
+#     "proxy_url": settings.PROXY_URL,
+#     "urllib3_proxy_kwargs": {
+#         "username": settings.PROXY_USERNAME,
+#         "password": settings.PROXY_PASSWORD,
+#     },
+# }
 
 
 if __name__ == "__main__":
